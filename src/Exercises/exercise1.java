ea23bbfbf58bc7eb5ae16a1d943cfa0652aba8aa
Sarina Salamon <salamon.sarina@gmail.com>
2022-01-27 20:01:59 +0000

    added exercise 1

--- src/Exercises/exercise1.java
+++ src/Exercises/exercise1.java
@@ -3,7 +3,22 @@ package Exercises;
 public class exercise1 {
     public static void main(String[] args) {
 
-        
+//Write a method which takes two numbers as arguments and prints their sum to the terminal.
+
+        sumNumbers(3,4);
+    }
+
+    public static void sumNumbers(int num1, int num2) {
+        int result = num1 + num2;
+        System.out.println( result + " is the answer");
 
     }
+
+
 }
+
+
+
+
+
+
